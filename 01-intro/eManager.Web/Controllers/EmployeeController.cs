6aa85a883f71006aec90a3c3253ba922abdd4a26
Nikita Starostin <n.starostin@itransition.com>
2017-07-21 11:58:01 +0300

    Add an employee functionality

--- 01-intro/eManager.Web/Controllers/EmployeeController.cs
+++ 01-intro/eManager.Web/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eManager.Domain;
+using eManager.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,39 @@ namespace eManager.Web.Controllers
 {
     public class EmployeeController : Controller
     {
-        public ActionResult Create()
+        private readonly IDepartmentDataSource _db;
+
+        public EmployeeController(IDepartmentDataSource db)
         {
-            return View();
+            _db = db;
+        }
+
+        public ActionResult Create(int departmentId)
+        {
+            var model = new CreateEmployeeViewModel();
+            model.DepartmentID = departmentId;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateEmployeeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = _db.Departments.Single(d => d.Id == model.DepartmentID);
+                var employee = new Employee()
+                {
+                    Name = model.Name,
+                    HireDate = model.HireDate
+                };
+                department.Employess.Add(employee);
+
+                _db.Save();
+
+                return RedirectToAction("detail", "department", new { id = model.DepartmentID });
+            }
+            return View(model);
         }
     }
 }
--- 01-intro/eManager.Web/Infrastructure/DepartmentDB.cs
+++ 01-intro/eManager.Web/Infrastructure/DepartmentDB.cs
@@ -32,5 +32,10 @@ namespace eManager.Web.Infrastructure
                 return Departments;
             }
         }
+
+        void IDepartmentDataSource.Save()
+        {
+            SaveChanges();
+        }
     }
 }
