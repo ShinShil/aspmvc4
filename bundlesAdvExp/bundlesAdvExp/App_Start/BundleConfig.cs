c4f930ce1343e28bc739e1c51c4ed48c0995ac3f
Nikita Starostin <n.starostin@itransition.com>
2017-07-25 15:09:18 +0300

    Bundle optimization, mvc.web.optimization, ServiceStack Bundler

---
+++ bundlesAdvExp/bundlesAdvExp/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace bundlesAdvExp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/Site.css")
+                .Include("~/Content/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-2.8.3.js"));
+        }       
+    }
+}
---
+++ bundlesAdvExp/bundlesAdvExp/Scripts/animal.coffee
@@ -0,0 +1,7 @@
+﻿class Animal:
+    say ->
+        console.log('OOO')
+
+($document).ready -> 
+    a = new Animal
+    a.say()
