e3d32f452a55f20c37b0370a38829b5ed1b18dfa
Nikita Starostin <n.starostin@itransition.com>
2017-07-26 11:31:43 +0300

    PUT, POST

--- 04-WebAPI/04-WebAPI/Controllers/VideosController.cs
+++ 04-WebAPI/04-WebAPI/Controllers/VideosController.cs
@@ -1,6 +1,7 @@
 ﻿using _04_WebAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -25,20 +26,57 @@ namespace _04_WebAPI.Controllers
         }
 
         // GET: api/Videos/5
-        public string Get(int id)
+        public Video Get(int id)
         {
-            return "value";
+            var video = db.Videos.Find(id);
+            if(video == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return video;
         }
 
         // POST: api/Videos
-        public void Post([FromBody]int id)
+        public HttpResponseMessage Post(Video video)
         {
-            
+            if(ModelState.IsValid)
+            {
+                db.Videos.Add(video);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, video);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = video.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
         // PUT: api/Videos/5
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage Put(int id, Video video)
         {
+            if(ModelState.IsValid && id == video.Id)
+            {
+                db.Entry(video).State = System.Data.Entity.EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch(DbUpdateConcurrencyException e)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, video);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
         // DELETE: api/Videos/5
