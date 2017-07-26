d5a86ac6c853718e5ad84764b41eb3e6d045f389
Nikita Starostin <n.starostin@itransition.com>
2017-07-26 14:50:28 +0300

    display modes

---
+++ 06-mobile/MvcMobile/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace MvcMobile
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DisplayModeProvider.Instance.Modes.Insert(DisplayModeProvider.Instance.Modes.Count - 1, new DefaultDisplayMode("customMode")
+            {
+                ContextCondition = c => true
+            });
+        }
+    }
+}
